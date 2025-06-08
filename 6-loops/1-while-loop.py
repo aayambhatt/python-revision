c078db4b2176fc4c840bba91e8bd643cf90e32b5
aayambhatt <aayambhatt23@gmail.com>
2025-06-08 22:41:57 +0530

    while-for-loop

---
+++ 6-loops/1-while-loop.py
@@ -0,0 +1,11 @@
+pet_name = "tofu"
+counter = 0
+
+while counter<5:
+    print("Tofu")
+    counter+=1
+
+print("____________________________________________________") 
+
+for i in range(5):
+    print("Bheeru")
